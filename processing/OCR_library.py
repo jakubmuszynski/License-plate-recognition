f34ce0b9907b4fc3ba7ae90cae8e5e06952f6e62
Jakub Muszyński <39170322+jakubmuszynski@users.noreply.github.com>
2020-06-20 18:25:19 +0200

    more exceptions

--- processing/OCR_library.py
+++ processing/OCR_library.py
@@ -33,6 +33,7 @@ def createValidContoursList(npaContours):
     return validContoursWithData
 
 def recognize(image, kNearest, validContoursWithData):
+    previouspreviousChar = 'Q'
     previousChar = 'Q'
     counter = 0
     strFinalString = ""
@@ -44,15 +45,60 @@ def recognize(image, kNearest, validContoursWithData):
         npaROIResized = np.float32(npaROIResized)
         retval, npaResults, neigh_resp, dists = kNearest.findNearest(npaROIResized, k = 1)
         strCurrentChar = str(chr(int(npaResults[0][0])))
-        if counter == 3:
+
+        # first 2 symbols cant be numbers
+        if counter == 1 or counter == 2:
+            if strCurrentChar == '0':
+                strCurrentChar = 'O'
+
+        # 3rd symbol has to be 0 instead of O except for OPO, SZO, NGO, PKO, PGO, ZKO, ZLO
+        elif counter == 3:
             if strCurrentChar == 'O':
-                if previousChar != 'P' and previousChar != 'Z' and previousChar != 'G' and previousChar != 'K' and previousChar != 'L':
+                allow_change = True
+                # OPO
+                if previouspreviousChar == 'O':
+                    if previousChar == 'P':
+                        allow_change = False
+                # SZO
+                elif previouspreviousChar == 'S':
+                    if previousChar == 'Z':
+                        allow_change = False
+                # NGO
+                elif previouspreviousChar == 'N':
+                    if previousChar == 'G':
+                        allow_change = False
+                # PKO and PGO
+                elif previouspreviousChar == 'P':
+                    if previousChar == 'K':
+                        allow_change = False
+                    elif previousChar == 'G':
+                        allow_change = False
+                # ZKO and ZLO
+                elif previouspreviousChar == 'Z':
+                    if previousChar == 'K':
+                        allow_change = False
+                    elif previousChar == 'L':
+                        allow_change = False
+                if allow_change:
                     strCurrentChar = '0'
-        if counter >= 4:
-            if strCurrentChar == 'O':
-                strCurrentChar = '0'
+
+        # determine between O and zero based on average ratio
+        elif counter >= 4:
+            if strCurrentChar == 'O' or strCurrentChar == '0':
+                ratios = 0
+                for c in validContoursWithData:
+                    ratios = ratios + (c.intRectWidth / c.intRectHeight)
+                avgRatio = ratios / len(validContoursWithData)
+                ratio = contourWithData.intRectWidth / contourWithData.intRectHeight
+                if ratio > avgRatio:
+                    strCurrentChar = 'O'
+                else:
+                    strCurrentChar = '0'
+
         strFinalString = strFinalString + strCurrentChar
+        previouspreviousChar = previousChar
         previousChar = strCurrentChar
+
     print(strFinalString)
     return strFinalString
 
